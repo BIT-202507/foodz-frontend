faf1112458c727ca2246b4a96380a52d49488bd5
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2026-01-14 15:41:33 -0500

    [+] ng g c features/pages/products/product-new-form [+] product-new-form.*: Crea Formulario Reactivo que obtiene como un objeto JavaScript de todos los datos enviados a traves de los campos del formulario

--- src/app/features/pages/products/product-new-form/product-new-form.ts
+++ src/app/features/pages/products/product-new-form/product-new-form.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-new-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './product-new-form.html',
   styleUrl: './product-new-form.css',
 })
 export class ProductNewForm {
+  // Atributo para almacenar los datos del formulario
+  formData!: FormGroup;
 
+  constructor() {
+    // Definido el formulario
+    this.formData = new FormGroup({
+      name: new FormControl(''),
+      description: new FormControl(''),
+      category: new FormControl(''),
+      type: new FormControl(''),
+      price: new FormControl(0),
+      image_url: new FormControl(''),
+      stock: new FormControl(0),
+      status: new FormControl(''),
+    });
+  }
+
+  onSubmit() {
+    // Lógica para manejar el envío del formulario
+    console.log(this.formData.value);
+  }
 }

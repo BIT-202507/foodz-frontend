2801de7d28b7c8ee4dc69cfd3755d1558bfa1893
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2026-01-15 00:13:21 -0500

    [+] app.config.ts: Implementa provideHttpClient withFetch [+] ng g s core/services/http-(product, category) [+] http-category.ts: Define getAllCategories GET /api/v1/categories Obtener categorias [+] product-new-component.ts: Inyecta dependencia HttpCategory e invoca en el hook ngOnInit para obtener todas las categorias del BackEnd

--- src/app/features/pages/products/product-new-form/product-new-form.ts
+++ src/app/features/pages/products/product-new-form/product-new-form.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HttpCategory } from '../../../../core/services/http-category';
 
 @Component({
   selector: 'app-product-new-form',
@@ -8,10 +9,12 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './product-new-form.css',
 })
 export class ProductNewForm {
+  edad: number = 0;   // Inferencia
+
   // Atributo para almacenar los datos del formulario
   formData!: FormGroup;
 
-  constructor() {
+  constructor( private httpCategory: HttpCategory ) {
     // Definido el formulario
     this.formData = new FormGroup({
       name: new FormControl(''),
@@ -28,5 +31,42 @@ export class ProductNewForm {
   onSubmit() {
     // Lógica para manejar el envío del formulario
     console.log(this.formData.value);
+  }
+
+  // Life Cicle Hooks
+  ngOnInit(): void {
+    // Lógica a ejecutar al inicializar el componente, solicita de datos, etc.
+    // console.log('ngOnInit');
+    this.httpCategory.getAllCategories().subscribe( data => {
+      console.log(data);
+    });
+  }
+  ngOnChanges(): void {
+    // Lógica a ejecutar cuando cambian las propiedades vinculadas a datos
+    console.log('ngOnChanges')
+  }
+  ngOnDestroy(): void {
+    // Lógica a ejecutar cuando se destruye el componente
+    console.log('ngOnDestroy')
+  }
+  ngDoCheck(): void {
+    // Lógica a ejecutar en cada ciclo de detección de cambios
+    console.log('ngDoCheck')
+  }
+  ngAfterContentInit(): void {
+    // Lógica a ejecutar después de inicializar el contenido del componente
+    console.log('ngAfterContentInit')
+  }
+  ngAfterContentChecked(): void {
+    // Lógica a ejecutar después de verificar el contenido del componente
+    console.log('ngAfterContentChecked')
+  }
+  ngAfterViewInit(): void {
+    // Lógica a ejecutar después de inicializar las vistas del componente
+    console.log('ngAfterViewInit')
+  }
+  ngAfterViewChecked(): void {
+    // Lógica a ejecutar después de verificar las vistas del componente
+    console.log('ngAfterViewChecked')
   }
 }

079c30d23b57a6a3e8275d42e185520abbf38ecc
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2026-01-16 21:25:27 -0500

    [+] user-list, user-(new,edit)-form: Crea los componentes del modulo de usuarios [+] app.routes.ts: Define las rutas para los componentes del modulo de usuarios [+] header.ts: Crea los enlaces para acceder a cada componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { Home } from './features/pages/home/home';
 import { Login } from './features/pages/login/login';
 import { Register } from './features/pages/register/register';
 import { PageNotFound } from './features/pages/page-not-found/page-not-found';
+import { UserList } from './features/pages/users/user-list/user-list';
+import { UserNewForm } from './features/pages/users/user-new-form/user-new-form';
+import { UserEditForm } from './features/pages/users/user-edit-form/user-edit-form';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
+  { path: 'dashboard/users', component: UserList },
+  { path: 'dashboard/user/new', component: UserNewForm },
+  { path: 'dashboard/user/edit/:id', component: UserEditForm },
   { path: '404', component: PageNotFound },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: '404', pathMatch: 'full' },

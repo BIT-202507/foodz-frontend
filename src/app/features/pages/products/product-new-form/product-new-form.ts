d428200a58643f67ddc01c8c27d1346ee57f920c
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2026-01-19 23:14:26 -0500

    [+] product-new-form: Validaciones y mensajes de error a los campos del formulario

--- src/app/features/pages/products/product-new-form/product-new-form.ts
+++ src/app/features/pages/products/product-new-form/product-new-form.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpCategory } from '../../../../core/services/http-category';
-import { LowerCasePipe } from '@angular/common';
+import { JsonPipe, LowerCasePipe } from '@angular/common';
 import { HttpProduct } from '../../../../core/services/http-product';
 
 @Component({
   selector: 'app-product-new-form',
-  imports: [ReactiveFormsModule, LowerCasePipe],
+  imports: [ReactiveFormsModule, LowerCasePipe, JsonPipe],
   templateUrl: './product-new-form.html',
   styleUrl: './product-new-form.css',
 })
@@ -24,23 +24,53 @@ export class ProductNewForm {
   ) {
     // Definido el formulario
     this.formData = new FormGroup({
-      name: new FormControl(''),
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
       description: new FormControl(''),
-      category: new FormControl(''),
-      type: new FormControl(''),
-      price: new FormControl(0),
-      image_url: new FormControl(''),
-      stock: new FormControl(0),
+      category: new FormControl('', [Validators.required] ),
+      type: new FormControl('dish', [Validators.required] ),
+      price: new FormControl(0, [Validators.required, Validators.min(0)] ),
+      image_url: new FormControl('', [Validators.required, Validators.minLength(10)] ),
+      stock: new FormControl(0, [Validators.required, Validators.min(1)] ),
       status: new FormControl('active'),
     });
   }
 
   onSubmit() {
-    // Lógica para manejar el envío del formulario
-    // console.log(this.formData.value);
-    // TODO: Cambiar el Callback por el objeto Observable
-    this.httpProduct.createProduct(this.formData.value).subscribe( (response) => {
-      console.log('Producto creado:', response);
+    // Verifica que el formulario sea válido antes de enviarlo
+    if( this.formData.valid ) {
+      // Lógica para manejar el envío del formulario
+      console.log('Envia estos datos al servicio', this.formData.value);
+      // Cambiar el Callback por el objeto Observable
+      this.httpProduct.createProduct(this.formData.value).subscribe({
+        next: ( data ) => {
+          console.log( 'Crea producto exitosamente', data );
+        },
+        error: ( err ) => {
+          console.error( 'Error al crear el producto', err );
+        },
+        complete: () => {
+          console.log( 'Solicitud de creación de producto completada' );
+          this.formData.reset(); // Reiniciar el formulario después de la creación exitosa
+        }
+      });
+    } else {
+      console.error('Formulario no válido');
+      // Marcar todos los campos como tocados para mostrar los errores de validación
+      // this.formData.markAllAsTouched();
+    }
+  }
+
+  onReset() {
+    // Establecer los valores iniciales del formulario
+    this.formData.setValue({
+      name: '',
+      description: '',
+      category: '',
+      type: 'dish',
+      price: 0,
+      image_url: '',
+      stock: 0,
+      status: 'active',
     });
   }
 
@@ -48,10 +78,15 @@ export class ProductNewForm {
   ngOnInit(): void {
     // Lógica a ejecutar al inicializar el componente, solicita de datos, etc.
     // console.log('ngOnInit');
-    // TODO: Cambiar el Callback por el objeto Observable
-    this.httpCategory.getAllCategories().subscribe( (data: any) => {
-      console.log(data.categories);
-      this.categories = data.categories;
+    // Cambiar el Callback por el objeto Observable
+    this.httpCategory.getAllCategories().subscribe({
+      next: ( data: any ) => {
+        console.log(data.categories);
+        this.categories = data.categories;
+      },
+      error: ( err: any ) => {
+        console.error( 'Error al obtener las categorías', err );
+      }
     });
   }
   ngOnChanges(): void {

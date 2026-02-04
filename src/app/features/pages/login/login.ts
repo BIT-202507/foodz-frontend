e8ee8de6e267877da7c106a3ae433a4b53447ee6
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2026-02-03 20:43:57 -0500

    [+] register: Crea formulario Reactivo que obtiene los datos [+] login: CRea formulario Reactivo que obtiene los datos

--- src/app/features/pages/login/login.ts
+++ src/app/features/pages/login/login.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { Counter } from "../../components/counter/counter";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [Counter],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  formData!: FormGroup;
 
+  constructor() {
+    this.formData = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email] ),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)])
+    });
+  }
+
+  onSubmit() {
+    if(this.formData.valid) {
+      console.log(this.formData.value);
+      this.formData.reset();
+    } else {
+      console.log("Form is invalid");
+      this
+    }
+  }
+
+  onReset() {
+    this.formData.reset();
+    this.formData.markAsPristine();
+  }
 }
--- src/app/features/pages/register/register.ts
+++ src/app/features/pages/register/register.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  formData!: FormGroup;
 
+  constructor() {
+    this.formData = new FormGroup({
+      name: new FormControl('', [Validators.required] ),
+      email: new FormControl('', [Validators.required, Validators.email] ),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    });
+  }
+
+  onSubmit() {
+    if(this.formData.valid) {
+      console.log(this.formData.value);
+      this.formData.reset();
+    } else {
+      console.log("Form is invalid");
+      this
+    }
+  }
+
+  onReset() {
+    this.formData.reset();
+    this.formData.markAsPristine();
+  }
 }

1b5d1ca055da772745f910b0dbb7855dfa353451
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2026-02-08 17:39:02 -0500

    [-] products: Elimina componentes, rutas y enlaces

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,6 @@ import { Home } from './features/pages/home/home';
 import { Login } from './features/pages/login/login';
 import { Register } from './features/pages/register/register';
 import { PageNotFound } from './features/pages/page-not-found/page-not-found';
-import { ProductList } from './features/pages/products/product-list/product-list';
-import { ProductNewForm } from './features/pages/products/product-new-form/product-new-form';
-import { ProductEditForm } from './features/pages/products/product-edit-form/product-edit-form';
 import { UserList } from './features/pages/users/user-list/user-list';
 import { UserNewForm } from './features/pages/users/user-new-form/user-new-form';
 import { UserEditForm } from './features/pages/users/user-edit-form/user-edit-form';
@@ -20,11 +17,8 @@ export const routes: Routes = [
   { path: 'register', component: Register },
   { path: '404', component: PageNotFound },
   { path: 'dashboard', component: Dashboard },
-  { path: 'dashboard/products', component: ProductList },
   { path: 'dashboard/users', component: UserList },
   { path: 'dashboard/categories', component: CategoryList },
-  { path: 'dashboard/product/new', component: ProductNewForm },
-  { path: 'dashboard/product/edit', component: ProductEditForm },
   { path: 'dashboard/users/new', component: UserNewForm },
   { path: 'dashboard/users/edit', component: UserEditForm },
   { path: 'dashboard/categories/new', component: CategoryNewForm },

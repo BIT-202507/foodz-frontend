2018d2364e91a181a63daeb72d9ffb48c6c75d84
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2026-02-11 20:15:58 -0500

    [+] http-product.ts: Define getProducts() [+] product-list.*: Itera la lista de productos

--- src/app/core/services/http-product.ts
+++ src/app/core/services/http-product.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, map, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +12,16 @@ export class HttpProduct {
     // Lógica para crear un nuevo producto
     // console.log(productData);
     return this.http.post<any>('http://localhost:3000/api/v1/products', productData);
+  }
+
+  getProducts() {
+    // Lógica para obtener la lista de productos
+    return this.http.get<any>('http://localhost:3000/api/v1/products').pipe(
+      map((response: any) => response.products),   // Ajusta esto según la estructura de tu respuesta
+      catchError((error) => {
+        console.error('Error al obtener productos', error);
+        return throwError(() => new Error('Error al obtener productos'));
+      })
+    );
   }
 }
--- src/app/features/pages/products/product-list/product-list.ts
+++ src/app/features/pages/products/product-list/product-list.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { HttpProduct } from '../../../../core/services/http-product';
+import { BehaviorSubject, Observable, switchMap } from 'rxjs';
+import { AsyncPipe, CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [ AsyncPipe, CurrencyPipe ],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
 export class ProductList {
 
+  // Atributo para almacenar la lista de productos
+  public products: Observable<any[]> = new Observable<any[]>();
+  // Atributo para controlar la actualización de la lista de productos
+  private refreshProductsTrigger$: BehaviorSubject<void> = new BehaviorSubject<void>(undefined);
+
+  constructor( private httpProduct: HttpProduct ) {}
+
+  ngOnInit() {
+    this.products = this.refreshProductsTrigger$.pipe(
+      switchMap(() => this.httpProduct.getProducts())
+    );
+  }
 }

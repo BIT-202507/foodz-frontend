1bda36286bcda1ab5430a2d7b03d940cf767e59f
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2026-02-19 21:29:38 -0500

    [+] dashboard.routes.ts: Agrupa las rutas por su slug

--- src/app/features/pages/dashboard/dashboard.routes.ts
+++ src/app/features/pages/dashboard/dashboard.routes.ts
@@ -4,55 +4,60 @@ import { roleGuard } from '../../../core/guards/role-guard';
 export const DASHBOARD_ROUTES: Routes = [
     {
         path: 'products',
-        loadComponent: () => import('../products/product-list/product-list'),
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('../products/product-list/product-list'),
+            },
+            {
+                path: 'new',
+                loadComponent: () => import('../products/product-new-form/product-new-form'),
+                data: { roles: ['admin'] }
+            },
+            {
+                path: 'edit/:id',
+                loadComponent: () => import('../products/product-edit-form/product-edit-form'),
+                data: { roles: ['admin'] }
+            }
+        ],
         canActivate: [roleGuard],
         data: { roles: ['admin', 'colaborator'] }
     },
     {
         path: 'users',
-        loadComponent: () => import('../users/user-list/user-list'),
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('../users/user-list/user-list'),
+            },
+            {
+                path: 'new',
+                loadComponent: () => import('../users/user-new-form/user-new-form'),
+            },
+            {
+                path: 'edit',
+                loadComponent: () => import('../users/user-edit-form/user-edit-form'),
+            }
+        ],
         canActivate: [roleGuard],
         data: { roles: ['admin'] }
     },
     {
         path: 'categories',
-        loadComponent: () => import('../categories/category-list/category-list'),
-        canActivate: [roleGuard],
-        data: { roles: ['admin', 'colaborator'] }
-    },
-    {
-        path: 'product/new',
-        loadComponent: () => import('../products/product-new-form/product-new-form'),
-        canActivate: [roleGuard],
-        data: { roles: ['admin'] }
-    },
-    {
-        path: 'product/edit/:id',
-        loadComponent: () => import('../products/product-edit-form/product-edit-form'),
-        canActivate: [roleGuard],
-        data: { roles: ['admin'] }
-    },
-    {
-        path: 'users/new',
-        loadComponent: () => import('../users/user-new-form/user-new-form'),
-        canActivate: [roleGuard],
-        data: { roles: ['admin'] }
-    },
-    {
-        path: 'users/edit',
-        loadComponent: () => import('../users/user-edit-form/user-edit-form'),
-        canActivate: [roleGuard],
-        data: { roles: ['admin'] }
-    },
-    {
-        path: 'categories/new',
-        loadComponent: () => import('../categories/category-form/category-form'),
-        canActivate: [roleGuard],
-        data: { roles: ['admin', 'colaborator'] }
-    },
-    {
-        path: 'categories/edit/:id',
-        loadComponent: () => import('../categories/category-form/category-form'),
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('../categories/category-list/category-list'),
+            },
+            {
+                path: 'new',
+                loadComponent: () => import('../categories/category-form/category-form'),
+            },
+            {
+                path: 'edit/:id',
+                loadComponent: () => import('../categories/category-form/category-form'),
+            }
+        ],
         canActivate: [roleGuard],
         data: { roles: ['admin', 'colaborator'] }
     },
